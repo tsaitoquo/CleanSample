f9d68ceb23863676b07913d3fbbabaec76fe36e8
Takeshi SAITO <t.saito@quo-digital.jp>
2018-09-14 23:19:52 +0900

    エラー発生時にスナックバーで通知する処理を追加

--- app/src/main/java/jp/travolta/cleansample/ui/post/PostListActivity.kt
+++ app/src/main/java/jp/travolta/cleansample/ui/post/PostListActivity.kt
@@ -1,8 +1,11 @@
 package jp.travolta.cleansample.ui.post
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import jp.travolta.cleansample.R
@@ -12,6 +15,8 @@ class PostListActivity: AppCompatActivity() {
     private lateinit var binding: ActivityPostListBinding
     private lateinit var viewModel: PostListViewModel
 
+    private var errorSnackbar: Snackbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
@@ -19,6 +24,19 @@ class PostListActivity: AppCompatActivity() {
         binding.postList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         viewModel = ViewModelProviders.of(this).get(PostListViewModel::class.java)
+        viewModel.errorMessage.observe(this, Observer { errorMessage ->
+            if(errorMessage != null) showError(errorMessage) else hideError()
+        })
         binding.viewModel = viewModel
+    }
+
+    private fun showError(@StringRes errorMessage:Int){
+        errorSnackbar = Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_INDEFINITE)
+        errorSnackbar?.setAction(R.string.retry, viewModel.errorClickListener)
+        errorSnackbar?.show()
+    }
+
+    private fun hideError(){
+        errorSnackbar?.dismiss()
     }
 }
--- app/src/main/java/jp/travolta/cleansample/ui/post/PostListViewModel.kt
+++ app/src/main/java/jp/travolta/cleansample/ui/post/PostListViewModel.kt
@@ -1,21 +1,30 @@
 package jp.travolta.cleansample.ui.post
 
 import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 import android.view.View
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import jp.travolta.cleansample.R
 import jp.travolta.cleansample.base.BaseViewModel
 import jp.travolta.cleansample.network.PostApi
 import javax.inject.Inject
 
 class PostListViewModel: BaseViewModel() {
+
+    companion object {
+        val TAG: String = PostListViewModel::class.java.simpleName
+    }
+
     @Inject
     lateinit var postApi: PostApi
 
     private lateinit var subscription: Disposable
 
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+    val errorMessage:MutableLiveData<Int> = MutableLiveData()
+    val errorClickListener = View.OnClickListener { loadPosts() }
 
     init{
         loadPosts()
@@ -39,7 +48,9 @@ class PostListViewModel: BaseViewModel() {
     }
 
     private fun onRetrievePostListStart(){
+        Log.d(TAG, "onRetrievePostListStart")
         loadingVisibility.value = View.VISIBLE
+        errorMessage.value = null
     }
 
     private fun onRetrievePostListFinish(){
@@ -47,10 +58,12 @@ class PostListViewModel: BaseViewModel() {
     }
 
     private fun onRetrievePostListSuccess(){
+        Log.d(TAG, "onRetrievePostListSuccess")
 
     }
 
     private fun onRetrievePostListError(){
-
+        Log.d(TAG, "onRetrievePostListError")
+        errorMessage.value = R.string.post_error
     }
 }
