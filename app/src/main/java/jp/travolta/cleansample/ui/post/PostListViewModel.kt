c6ca726b9075ed3709532453af63eeb7fed8a3eb
Takeshi SAITO <t.saito@quo-digital.jp>
2018-09-14 22:12:49 +0900

    ViewModelの簡易実装まで

--- app/src/main/java/jp/travolta/cleansample/ui/post/PostListViewModel.kt
+++ app/src/main/java/jp/travolta/cleansample/ui/post/PostListViewModel.kt
@@ -1,5 +1,10 @@
 package jp.travolta.cleansample.ui.post
 
+import android.arch.lifecycle.MutableLiveData
+import android.view.View
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import jp.travolta.cleansample.base.BaseViewModel
 import jp.travolta.cleansample.network.PostApi
 import javax.inject.Inject
@@ -7,4 +12,45 @@ import javax.inject.Inject
 class PostListViewModel: BaseViewModel() {
     @Inject
     lateinit var postApi: PostApi
+
+    private lateinit var subscription: Disposable
+
+    val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+
+    init{
+        loadPosts()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
+    private fun loadPosts(){
+        subscription = postApi.getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onRetrievePostListStart() }
+                .doOnTerminate { onRetrievePostListFinish() }
+                .subscribe(
+                        { onRetrievePostListSuccess() },
+                        { onRetrievePostListError() }
+                )
+    }
+
+    private fun onRetrievePostListStart(){
+        loadingVisibility.value = View.VISIBLE
+    }
+
+    private fun onRetrievePostListFinish(){
+        loadingVisibility.value = View.GONE
+    }
+
+    private fun onRetrievePostListSuccess(){
+
+    }
+
+    private fun onRetrievePostListError(){
+
+    }
 }
