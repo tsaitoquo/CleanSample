a489789acbc5de35d799450062ace315a2edd5f2
Takeshi SAITO <t.saito@quo-digital.jp>
2018-09-14 16:25:49 +0900

    Daggar2導入、ViewModelへのDIまで実装

--- app/src/main/java/jp/travolta/cleansample/base/BaseViewModel.kt
+++ app/src/main/java/jp/travolta/cleansample/base/BaseViewModel.kt
@@ -1,7 +1,10 @@
 package jp.travolta.cleansample.base
 
 import android.arch.lifecycle.ViewModel
+import jp.travolta.cleansample.network.PostApi
+import javax.inject.Inject
 
 class BaseViewModel: ViewModel() {
-
+    @Inject
+    lateinit var postApi: PostApi
 }
