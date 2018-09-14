326b11322eb97e8b1718a36ead52e8b2d19118c2
Takeshi SAITO <t.saito@quo-digital.jp>
2018-09-14 22:05:26 +0900

    DaggerによるDIまで実装

--- app/src/main/java/jp/travolta/cleansample/base/BaseViewModel.kt
+++ app/src/main/java/jp/travolta/cleansample/base/BaseViewModel.kt
@@ -1,8 +1,29 @@
 package jp.travolta.cleansample.base
 
 import android.arch.lifecycle.ViewModel
+import jp.travolta.cleansample.injection.component.DaggerViewModelInjector
+import jp.travolta.cleansample.injection.component.ViewModelInjector
+import jp.travolta.cleansample.injection.module.NetworkModule
 import jp.travolta.cleansample.network.PostApi
+import jp.travolta.cleansample.ui.post.PostListViewModel
 import javax.inject.Inject
 
 abstract class BaseViewModel: ViewModel() {
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+            .builder()
+            .networkModule(NetworkModule)
+            .build()
+
+    init {
+        inject()
+    }
+
+    /**
+     * Injects the required dependencies
+     */
+    private fun inject() {
+        when (this) {
+            is PostListViewModel -> injector.inject(this)
+        }
+    }
 }
---
+++ app/src/main/java/jp/travolta/cleansample/injection/component/ViewModelInjector.kt
@@ -0,0 +1,26 @@
+package jp.travolta.cleansample.injection.component
+
+import dagger.Component
+import jp.travolta.cleansample.injection.module.NetworkModule
+import jp.travolta.cleansample.ui.post.PostListViewModel
+import javax.inject.Singleton
+
+/**
+ * Component providing inject() methods for presenters.
+ */
+@Singleton
+@Component(modules = [(NetworkModule::class)])
+interface ViewModelInjector {
+    /**
+     * Injects required dependencies into the specified PostListViewModel.
+     * @param postListViewModel PostListViewModel in which to inject the dependencies
+     */
+    fun inject(postListViewModel: PostListViewModel)
+
+    @Component.Builder
+    interface Builder {
+        fun build(): ViewModelInjector
+
+        fun networkModule(networkModule: NetworkModule): Builder
+    }
+}
