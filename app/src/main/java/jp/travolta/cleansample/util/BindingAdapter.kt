1bcb7f83568c530134a099311edbf0b53c5386ef
Takeshi SAITO <t.saito@quo-digital.jp>
2018-09-14 22:48:33 +0900

    DataBindingの処理を実装

---
+++ app/src/main/java/jp/travolta/cleansample/util/BindingAdapter.kt
@@ -0,0 +1,16 @@
+package jp.travolta.cleansample.util
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.databinding.BindingAdapter
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import jp.travolta.cleansample.util.extension.getParentActivity
+
+@BindingAdapter("mutableVisibility")
+fun setMutableVisibility(view: View, visibility: MutableLiveData<Int>?) {
+    val parentActivity: AppCompatActivity? = view.getParentActivity()
+    if(parentActivity != null && visibility != null) {
+        visibility.observe(parentActivity, Observer { value -> view.visibility = value?:View.VISIBLE})
+    }
+}
